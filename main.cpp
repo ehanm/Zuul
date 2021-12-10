61fd7ee76aaec452a0c6569cbaf90b3452232797
U-BSD\452735 <452735@SHS-10D9C064E.beaverton.k12.or.us>
2021-12-10 14:25:00 -0800

    zuul files and initial narrative

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "room.h"
+
+using namespace std
+
+int main(){
+
+  cout << "Welcome to Zuul. You are in a haunted house. ooooohhh spooky!" << endl;
+  cout << "" << endl;
+  cout << "Your job is to collect the legendary utensil set: the spoon, the fork, the knife, the chopsticks, and the spork." << endl;
+  cout << "" << endl;
+  cout << "Why?" << endl;
+  cout << "..." << endl;
+  cout << "I don't know actually, just go get them." << endl;
+
+  bool cont = true;
+
+  while (cont == true){
+
+    
+
+
+    
+  }
+  
+
+}
