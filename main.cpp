4347979f43e63eff24c9fe65e3dfc9233842d1ad
ehanm <452735@bsd48.org>
2021-12-16 02:40:24 +0000

    made solid progress on directions and room printing

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@ int main(){
   map<int, Room> hauntedhouse;
   vector<Room*> r;
   char a[10000];
+  bool cont = true;
+  bool checkdirection = false;
+  int locationval = 0;
   // an entrance
   // a grand hall
   // the ability to move between the 2
@@ -28,31 +31,86 @@ int main(){
   cout << "Why?" << endl;
   cout << "..." << endl;
   cout << "I don't know actually, just go get them." << endl;
-
-  bool cont = true;
-
+  cout << "" << endl;
+  cout << "" << endl;
+  
   while (cont == true){
 
-    cout << (*it)->description;
+    for (int i = 0; i < r.size(); i++){
+      if (i == locationval){
+	cout << r[i]->description << endl;
+	cout << "" << endl;
+	cout << "Objects in the room (will be blank if nothing exists): ";
+	cout << r[i]->item << endl;
+      }
+    }
+
+    checkdirection = false;
+
+    while (checkdirection == false) {
+
     cout << "Where do you want to go?" << endl;
-    cin >> a;
 
-    cout << "Available directions: " << endl;
+    cout << "Available directions: ";
 
-    if ((*it)->exitN == true){
-      cout << "North ";
+    if (r[locationval]->exitN == true){
+      cout << "NORTH ";
     }
-    if ((*it)->exitS == true){
-      cout << "South ";
+    if (r[locationval]->exitS == true){
+      cout << "SOUTH ";
     }
-    if ((*it)->exitE == true){
-      cout << "East ";
+    if (r[locationval]->exitE == true){
+      cout << "EAST ";
     }
-    if ((*it)->exitW == true){
-      cout << "West ";
+    if (r[locationval]->exitW == true){
+      cout << "WEST ";
     }
     cout << "" << endl;
 
+    cin >> a;    
+
+    
+    if (strcmp(a, "NORTH") == 0){
+      if (r[locationval]->exitN == false){
+	cout << "invalid direction!" << endl;
+      }
+      else if (r[locationval]->exitN == true) {
+	// move north
+	checkdirection = true;
+      }
+    }
+    else if (strcmp(a, "SOUTH") == 0){
+      if (r[locationval]->exitS == false){
+	cout << "invalid direction!" << endl;
+      }
+      else if (r[locationval]->exitS == true) {
+	// move south
+	checkdirection = true;
+      }
+    }
+    else if (strcmp(a, "EAST") == 0){
+      if (r[locationval]->exitE == false){
+	cout << "invalid direction!" << endl;
+      }
+      else if (r[locationval]->exitE == true) {
+	// move east
+	checkdirection = true;
+      }
+    }
+    else if (strcmp(a, "WEST") == 0){
+      if (r[locationval]->exitW == false){
+	cout << "invalid direction!" << endl;
+      }
+      else if (r[locationval]->exitW == true) {
+	// move west
+	checkdirection = true;
+      }
+    }
+    else {
+      cout << "invalid input! (make sure your direction is in all caps)" << endl;
+    }
+    
+    }
 
     if (strcmp(a, "PRINTROOMS") == 0){
       for (it = r.begin(); it < r.end(); it++){
@@ -91,28 +149,68 @@ int main(){
 void makeRooms(vector<Room*> &r){
 
   Room* rp = new Room();
-  // entrance
 
   char input[1000];
 
-  strcpy(rp->place, "entrance");
-  
-  strcpy(rp->description, "You are at the entrance of the haunted house! You feel a pretty nervous, but the\
- legendary utensil set sounds pretty cool...");
 
-  strcpy(rp->item, "SPOON");
-  
-  rp->exitN = true;
-  rp->exitS = false;
-  rp->exitE = false;
-  rp->exitW = false;
-  (r).push_back(rp);
+  // laundry = 14
 
-  // grand hall
+
+  // bedroom = 13
+
+  
+  // rec room = 12
+
+  
+  // hallway = 11
+  
+
+  // stairs = 10  
+
+
+  // pool = 9
+
+
+  // backyard = 8
+
+  
+  // den = 7
+  
+
+  // kitchen = 6
+  
+
+  // storage = 5
+  
+
+  // dining = 4
+  
+
+  // lavatory = 3
+
+  
+  // study = 2
+
+  
+  // grand hall = 1
   strcpy(rp->place, "grand hall");
   strcpy(rp->description, "You are in the grand hall. (add more detail)");
   strcpy(rp->item, "");
   (r).push_back(rp);
+
+
+  // entrance = 0
+  strcpy(rp->place, "entrance");
+  strcpy(rp->description, "You are at the entrance of the haunted house! You feel a pretty nervous, but the\
+ legendary utensil set sounds pretty cool...");
+  strcpy(rp->item, "SPOON");
+
+  rp->exitN = true;
+  rp->exitS = false;
+  rp->exitE = false;
+  rp->exitW = false;
+  r.push_back(rp);
+
 
  
   
